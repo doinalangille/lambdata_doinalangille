a7af32bd526f95155e7df590c7469e02fb28a76a
Doina Langille <doina.jitoreanu@gmail.com>
2020-02-11 01:59:54 -0500

    Init and df_utils files edited

---
+++ lambdata_doinalangille/__init__.py
@@ -0,0 +1,10 @@
+"""
+lambdata - a collection of Data Science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+# sample code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
